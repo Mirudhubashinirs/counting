bfa371ed6cf8301114f41d80886974a158faf25e
Mirudhu <mirudhubashini.ramasamysridharan002@stud.fh-dortmund.de>
2024-04-01 01:18:11 +0200

    add gpu to triton

--- 3_docker_triton_run.py
+++ 3_docker_triton_run.py
@@ -4,15 +4,16 @@ import contextlib
 from tritonclient.http import InferenceServerClient
 
 # Define image https://catalog.ngc.nvidia.com/orgs/nvidia/containers/tritonserver
-tag = 'nvcr.io/nvidia/tritonserver:23.09-py3'  # 6.4 GB
+tag = 'nvcr.io/nvidia/tritonserver:23.09-py3'
 
 # Pull the image
 subprocess.call(f'docker pull {tag}', shell=True)
 
 # Run the Triton server and capture the container ID
-triton_repo_path = "/home/loki/PycharmProjects/counting/tmp/triton_repo"
+triton_repo_path = "/home/mirudhu/PycharmProjects/obj_counting/tmp/triton_repo"
 container_id = subprocess.check_output(
-    f'docker run -d --rm -v {triton_repo_path}:/models -p 7000:7000 {tag} tritonserver --model-repository=/models',
+#    f'docker run -d --rm -v {triton_repo_path}:/models -p8000:8000 {tag} tritonserver --model-repository=/models',
+    f'docker run -d --gpus device=0 --rm -v {triton_repo_path}:/models -p8000:8000 {tag} tritonserver --model-repository=/models',
     shell=True).decode('utf-8').strip()
 
 # Wait for the Triton server to start
