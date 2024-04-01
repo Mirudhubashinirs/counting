6a1162f81373dd9e6b20a51bcc094c54a4e4b461
Mirudhu <mirudhubashini.ramasamysridharan002@stud.fh-dortmund.de>
2024-04-01 18:08:26 +0200

    benchmark

---
+++ 5_run_inference_cpu.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+from ultralytics.utils.benchmarks import benchmark
+
+# Load the Triton Server model
+model = YOLO(f'http://localhost:8000/yolo', task='detect')
+
+# Run inference on the server
+results = model('/home/mirudhu/Documents/2024-02-20_17_53_04_119.jpg.4n4qoddt.ingestion-684cb69778-qb2ft.jpg')
+
+# Benchmark on CPU
+benchmark(model='/home/mirudhu/Documents/Counting/CVAT/CVAT_New/ep_m_model.pt', data='/home/mirudhu/Documents/Counting/CVAT/CVAT_New/detect/detect.yaml', imgsz=640, half=False, device="cpu", int8=True)
